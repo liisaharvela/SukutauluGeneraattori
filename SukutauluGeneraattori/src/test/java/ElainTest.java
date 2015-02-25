56127b6b4b978e33cb457dec0ad657a314ea9302
liisaharvela <liisa_harvela@yahoo.com>
2015-02-25 14:58:53 +0200

    Pomps

--- SukutauluGeneraattori/src/test/java/ElainTest.java
+++ SukutauluGeneraattori/src/test/java/ElainTest.java
@@ -4,41 +4,79 @@
  * and open the template in the editor.
  */
 
+/**
+ *
+ * @author Liisa
+ */
+import elaingeneraattori.sovelluslogiikka.Elain;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.junit.matchers.JUnitMatchers.either;
 
-/**
- *
- * @author Liisa
- */
 public class ElainTest {
-    
+
     public ElainTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void generaattoriTekeeErilaisiaElaimia() {
+        Elain elainYksi = new Elain();
+        Elain elainKaksi = new Elain();
+        
+        String eka = elainYksi.toString();
+        String toka = elainKaksi.toString();
+        
+        assertThat(eka, not(toka));
+    }
+
+    @Test
+    public void generaattorinElaimillaOnEriNimet() {
+        Elain elainYksi = new Elain();
+        String nimiYksi = elainYksi.getNimi();
+        Elain elainKaksi = new Elain();
+        String nimiKaksi = elainKaksi.getNimi();
+        assertThat(nimiYksi, not(nimiKaksi));
+    }
+    
+    @Test
+    public void generaattorinElaimillaOnEriSakakorkeudet() {
+        Elain elainYksi = new Elain();
+        int sakaYksi = elainYksi.getSakakorkeus();
+        Elain elainKaksi = new Elain();
+        int sakaKaksi = elainKaksi.getSakakorkeus();
+        assertThat(sakaYksi, not(sakaKaksi));
+    }
+    
+    @Test
+    public void generaattorinElaimetSaavatSukupuolen() {
+        Elain elainYksi = new Elain();
+        String skpYksi = elainYksi.getSkp();
+        
+        assertThat(skpYksi, either(containsString("uros")).or(containsString("naaras")));
+    }
+    
+    
+    
 }
